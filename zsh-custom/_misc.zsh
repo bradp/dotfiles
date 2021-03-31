4e890685666dd1d56ec27251ddac81409c150f15
Brad Parbs <brad@bradparbs.com>
2021-03-31 15:29:50 -0500

    Clean up comments

--- zsh-custom/_misc.zsh
+++ zsh-custom/_misc.zsh
@@ -1,12 +1,9 @@
-#########################################
-# Git                                   #
-#########################################
 # Runs `git pull` on every directory within the current directory
 function gitupdateall() {
 	find . -type d -print -maxdepth 1 -execdir git --git-dir={}/.git --work-tree=$PWD/{} pull \;
 }
 
-
+# Create a new chassis site
 function newchassis() {
 	git clone --recursive --single-branch --depth 1 https://github.com/Chassis/Chassis "$HOME"/Sites/"$1";
 	cd "$1" || exit;
