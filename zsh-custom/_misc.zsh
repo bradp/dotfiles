6f0e9561ca6d03278571475a2d73f444c0db8efb
Brad Parbs <brad@bradparbs.com>
2021-03-31 15:30:02 -0500

    Remove unused functions

--- zsh-custom/_misc.zsh
+++ zsh-custom/_misc.zsh
@@ -10,12 +10,3 @@ function newchassis() {
 	trash .git;
 	vagrant up --provision;
 }
-
-
-function cdf() {  # short for cdfinder
-  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
-}
-
-function ha() {
-	history | ag $@
-}
