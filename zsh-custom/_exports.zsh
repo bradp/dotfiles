9ccff2e47c78e8ad5308347ff180f933429615e5
Brad Parbs <brad@bradparbs.com>
2021-05-18 21:04:59 -0500

    Add fzf options

--- zsh-custom/_exports.zsh
+++ zsh-custom/_exports.zsh
@@ -41,3 +41,10 @@ export GOPATH="$HOME/go"
 export GPG_TTY=$(tty)
 
 export NEXT_TELEMETRY_DISABLED=1
+
+export FZF_DEFAULT_OPTS="--ansi \
+                        --height=90% --info=inline --border \
+                        --margin=1 --padding=1 \
+                        --color 'fg:#665c54,fg+:#ebdbb2,hl:#fe8019,bg:#282828,bg+:#3c3836,preview-bg:#223344,border:#fbf1c7' \
+                        "
+export FZF_COMPLETION_OPTS='--border --info=inline'
