837316868ee8422819199248fae7bbd615180a53
Brad Parbs <brad@bradparbs.com>
2021-07-15 08:51:14 -0500

    Clean up path aliases

--- zsh-custom/paths.zsh
+++ zsh-custom/paths.zsh
@@ -9,15 +9,10 @@
 # ░░       ░░░░░░░░    ░░  ░░   ░░ ░░░░░░
 #
 
-alias docs="cd ~/Documents"
-alias dl="cd ~/Downloads"
-alias dt="cd ~/Desktop"
 alias box="cd ~/Dropbox"
-alias sites="cd ~/Sites"
 alias dot="cd ~/dotfiles"
-
 alias goo="cd ~/go/src/github.com/bradp"
 alias godir="cd ~/go/src/github.com/bradp"
-
+alias sites="cd ~/Sites"
 alias ww="cd ~/Dropbox/Working"
 alias ws="cd ~/Dropbox/Working/sites"
