e7048c4836213074c75c55b0dd9b3f3bc3308973
Brad Parbs <brad@bradparbs.com>
2021-07-15 08:57:25 -0500

    Remove headers in exports.zsh

--- zsh-custom/exports.zsh
+++ zsh-custom/exports.zsh
@@ -8,28 +8,16 @@
 #	 ░░░░░░ ░░   ░░ ░░       ░░░░░░  ░░░       ░░  ░░░░░░
 #
 
-#########################################
-# Language                              #
-#########################################
 export LANG="en_US.UTF-8";
 export LC_ALL="en_US.UTF-8";
 
-#########################################
-# History                               #
-#########################################
 export HISTSIZE=32768;
 export HISTFILESIZE=$HISTSIZE;
 export HISTCONTROL=ignoredups;
 
-#########################################
-# Homebrew                              #
-#########################################
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HOMEBREW_NO_ANALYTICS=1
 
-#########################################
-# Config set                            #
-#########################################
 export EDITOR="vim";
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
