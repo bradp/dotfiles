fafa7206e35f4319f5ce76d0f38f5863f5a93b55
Brad Parbs <brad@bradparbs.com>
2021-07-15 09:07:25 -0500

    add bookmarks function

--- zsh-custom/functions.zsh
+++ zsh-custom/functions.zsh
@@ -87,4 +87,16 @@ function battery() {
 	echo "$percent"
 }
 
+function bookmarks() {
+     bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
+
+     jq_script='
+        def ancestors: while(. | length >= 2; del(.[-1,-2]));
+        . as $in | paths(.url?) as $key | $in | getpath($key) | {name,url, path: [$key[0:-2] | ancestors as $a | $in | getpath($a) | .name?] | reverse | join("/") } | .path + "/" + .name + "\t" + .url'
+
+    jq -r "$jq_script" < "$bookmarks_path" \
+        | sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' \
+        | fzf --ansi \
+        | cut -d$'\t' -f2 \
+        | xargs open
 }
