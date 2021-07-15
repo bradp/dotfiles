efb97bb46c28b420ba1afff1502044ebbde63106
Brad Parbs <brad@bradparbs.com>
2021-07-15 08:58:50 -0500

    Alphabetize exports

--- zsh-custom/exports.zsh
+++ zsh-custom/exports.zsh
@@ -8,8 +8,15 @@
 #	 ░░░░░░ ░░   ░░ ░░       ░░░░░░  ░░░       ░░  ░░░░░░
 #
 
-export LANG="en_US.UTF-8";
-export LC_ALL="en_US.UTF-8";
+export EDITOR="vim";
+
+export FZF_COMPLETION_OPTS='--border --info=inline'
+export FZF_DEFAULT_OPTS="--height=90% --info=inline --border --margin=1 --padding=1 --prompt='▶ ' --pointer='▶' --color 'fg:#665c54,fg+:#ebdbb2,hl:#fe8019,bg:#282828,bg+:#3c3836,preview-bg:#223344,border:#fbf1c7'"
+export GOPATH="$HOME/go"
+
+export GPG_TTY=$(tty)
+
+export GREP_OPTIONS="--color=auto";
 
 export HISTSIZE=32768;
 export HISTFILESIZE=$HISTSIZE;
@@ -18,21 +25,12 @@ export HISTCONTROL=ignoredups;
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HOMEBREW_NO_ANALYTICS=1
 
-export EDITOR="vim";
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export LANG="en_US.UTF-8";
+export LC_ALL="en_US.UTF-8";
 
-export GREP_OPTIONS="--color=auto";
 export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
-export GOPATH="$HOME/go"
-
-export GPG_TTY=$(tty)
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 export NEXT_TELEMETRY_DISABLED=1
 
-export FZF_DEFAULT_OPTS="--ansi \
-                        --height=90% --info=inline --border \
-                        --margin=1 --padding=1 \
-                        --color 'fg:#665c54,fg+:#ebdbb2,hl:#fe8019,bg:#282828,bg+:#3c3836,preview-bg:#223344,border:#fbf1c7' \
-                        "
-export FZF_COMPLETION_OPTS='--border --info=inline'
